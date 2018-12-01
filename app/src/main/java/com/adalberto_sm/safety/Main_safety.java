362026e542ebba954a25f6c621bc04858a2bef99
Adalkenny <adalbertmascarenhas1@gmail.com>
2018-12-01 14:44:19 -0100

    projeto para grupo paack

---
+++ app/src/main/java/com/adalberto_sm/safety/Main_safety.java
@@ -0,0 +1,13 @@
+package com.adalberto_sm.safety;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Main_safety extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_safety);
+    }
+}
